782265aa9af40b6ba1961ed3995e254c929e7d40
A.suzuki <suzu.devworks@gmail.com>
2024-03-24 09:09:15 +0000

    Use Container Station.

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
 # qnap-configurations
+
+- [qnap-configurations](#qnap-configurations)
+  - [Envisonment](#envisonment)
+  - [Create a container](#create-a-container)
+  - [Clone repository](#clone-repository)
+  - [Configuration Python](#configuration-python)
+    - [Use python3 in debian package](#use-python3-in-debian-package)
+    - [Use venv](#use-venv)
+  - [Scheduling](#scheduling)
+
+## Envisonment
+
+- QTS 5.1.5.2679(2024-02-29) on TS-231K
+
+## Create a container
+
+Create a container using Container Station: 
+
+1. Creating a container
+   - image: `debian:bookworm-slim`
+2. Container settings
+   - Advanced Settings
+   - \> Storage
+   - \> Add Volume (Bind path to mounted host)
+     - Host: /Multimedia
+     - Container: /mnt/Multimedia
+     - Mode: RW
+3. Create container 
+   - wait until running
+
+
+## Clone repository
+
+Enter the Docker container you just ran and start a console session:
+
+```shell
+docker exec -it <containerid> bash
+```
+
+First update apt. All commands after this are for root user:
+
+```shell
+apt update 
+```
+
+And I don't have git.
+
+```shell
+apt install -y git
+```
+
+Once you have git installed, you can finally clone it.
+
+```shell
+git clone https://github.com/suzu-devworks/qnap-configurations.git
+```
+
+## Configuration Python
+
+### Use python3 in debian package 
+
+All you need in python3 are the following three packages:
+
+```shell
+sudo apt install -y python3 python3-pillow
+```
+
+Check the python path settings in `move_photo_files.sh`
+
+```shell
+python=/usr/bin/python3
+```
+
+### Use venv
+
+All you need in python3 are the following three packages:
+
+```shell
+sudo apt install -y python3 python3-venv python3-pip
+```
+
+I used venv, but the packaged python might be faster.
+
+```shell
+python -m venv .venv
+. .venv/bin/activate
+
+python -m pip install --upgrade pip
+pip install -y pdm
+```
+
+Restore package.
+
+```shell
+pdm install
+```
+
+Check the python path settings in `move_photo_files.sh`
+
+```shell
+root_dir=$(cd $(dirname ${0})/../ && pwd)
+python=${root_dir}/.venv/bin/python
+```
+
+## Scheduling
+
+Crom and vim are also not included.
+
+```shell
+apt install -y cron vim
+```
+
+```shell
+export EDITOR=vi
+crontab -e
+```
+
+For example, if it starts every 12 hours:
+
+```crontab
+5 */12 * * * /home/admin/repos/qnap-configurations/bin/move_photo_files.sh > /dev/stdout 2>&1
+```
--- src/move_photo_files/move_photo_files.py
+++ src/move_photo_files/move_photo_files.py
@@ -22,32 +22,14 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 LOGGING_CONFIG = {
-    'version': 1,
-    'disable_exisiting_loggers': True,
-    'formatters': {
-        'default': {
-            'format': '%(asctime)s [%(levelname)-8s]: %(message)s'
-        }
+    "version": 1,
+    "disable_exisiting_loggers": True,
+    "formatters": {"default": {"format": "%(asctime)s [%(levelname)s]: %(message)s"}},
+    "handlers": {"console": {"class": "logging.StreamHandler", "level": "DEBUG", "formatter": "default"}},
+    "loggers": {
+        "": {"level": "WARNING", "handlers": ["console"], "propagate": False},
+        "__main__": {"level": "DEBUG", "handlers": ["console"], "propagate": False},
     },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'formatter': 'default'
-        }
-    },
-    'loggers': {
-        '': {
-            'level': 'WARNING',
-            'handlers': ['console'],
-            'propagate': False
-        },
-        '__main__': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False
-        }
-    }
 }
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = getLogger(__name__)
@@ -61,8 +43,8 @@ def find_photo_files(dir: str):
     Yields:
         Path: Path instance of photo file .
     """
-    ext_matcher = re.compile('.*\\.jpe?g\\Z', re.IGNORECASE)
-    for f in (d for d in Path(dir).glob('**/*') if (d.match('[!\\.]*/*') and ext_matcher.match(d.name))):
+    ext_matcher = re.compile(".*\\.jpe?g\\Z", re.IGNORECASE)
+    for f in (d for d in Path(dir).glob("**/*") if (d.match("[!\\.]*/*") and ext_matcher.match(d.name))):
         yield f
 
 
@@ -87,15 +69,15 @@ def get_exif_shooting_date(img: Image):
     Args:
         img : Image instance of photo.
 
-    Returns: 
+    Returns:
         datetime: EXIF shooting date (DateTimeOriginal).
     """
     exif = img._getexif()
     try:
         for id, val in exif.items():
             tg = TAGS.get(id, id)
-            if tg == 'DateTimeOriginal':
-                dt = datetime.strptime(val, '%Y:%m:%d %H:%M:%S')
+            if tg == "DateTimeOriginal":
+                dt = datetime.strptime(val, "%Y:%m:%d %H:%M:%S")
                 return dt
 
     except (AttributeError, ValueError) as e:
@@ -114,21 +96,20 @@ def move_file(source: Path, dest: Path):
     dest.mkdir(mode=0o777, parents=True, exist_ok=True)
 
     try:
-        #shutil.copy2(str(source), str(dest))
-        #shutil.move(str(source), str(dest))
-        logger.debug('move: {} -> {}'.format(source, dest))
+        # shutil.copy2(str(source), str(dest))
+        # shutil.move(str(source), str(dest))
+        logger.debug("move: {} -> {}".format(source, dest))
 
-    except (shutil.Error) as e:
+    except shutil.Error as e:
         logger.warning(repr(e))
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Organize and move photo files into folders.')
-    parser.add_argument('-o', '--out', default=None,
-                        help='destination folder path.(default src_path)', dest='dest_path')
-    parser.add_argument('src_path',
-                        help='source folder path.')
+    parser = argparse.ArgumentParser(description="Organize and move photo files into folders.")
+    parser.add_argument(
+        "-o", "--out", default=None, help="destination folder path.(default src_path)", dest="dest_path"
+    )
+    parser.add_argument("src_path", help="source folder path.")
 
     args = parser.parse_args()
 
@@ -136,27 +117,26 @@ def main():
     dest_path_root = Path(args.dest_path or args.src_path)
 
     try:
-        logger.info('start.')
-        logger.info('src_path:  {}'.format(source_path))
-        logger.info('dest_path: {}'.format(dest_path_root))
+        logger.info('"{}" - started.'.format(Path(__file__).name))
+        logger.info("src_path:  {}".format(source_path))
+        logger.info("dest_path: {}".format(dest_path_root))
 
         for path in find_photo_files(source_path):
             date_of_shooting = do_operation_image(path, get_exif_shooting_date)
             if not isinstance(date_of_shooting, datetime):
-                logger.warning(
-                    'can not get the shooting date of EXIF: {}'.format(path))
+                logger.warning("can not get the shooting date of EXIF: {}".format(path))
                 continue
 
-            dest_path = dest_path_root / date_of_shooting.strftime('%Y/%Y-%m')
+            dest_path = dest_path_root / date_of_shooting.strftime("%Y/%Y-%m")
 
             if isinstance(dest_path, Path):
                 move_file(path, dest_path)
 
-        logger.info('end.')
+        logger.info('"{}" - finished.\n'.format(Path(__file__).name))
 
     except Exception:
-        logger.exception('Unhandled.')
+        logger.exception("Unhandled.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
